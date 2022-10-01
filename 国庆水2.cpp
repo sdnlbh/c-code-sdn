0a32cde88df470781912e5d1e00f4bbaaf0b3c87
sdnlbh <113505317+sdnlbh@users.noreply.github.com>
2022-10-01 23:56:51 +0800

    国庆水2

---
+++ 国庆水2.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+int main()
+{   
+	char arr[] = {"Happy National Day"};
+	char arr2[19] = {" "};
+	int sz = sizeof(arr) / sizeof(arr[0]) - 1;
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		arr2 [left] = arr [left];
+		arr2[right] = arr[right];
+		printf("%s", arr2);
+		Sleep(1000);
+		system("cls");
+		left++;
+		right--;
+	}
+
+	return 0;
+}
