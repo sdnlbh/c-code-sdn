5f60b1a097ea66a01cf090f05128b6aaf14bd519
sdnlbh <113505317+sdnlbh@users.noreply.github.com>
2022-11-03 22:13:26 +0800

    Add files via upload

---
+++ strncat.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+
+char* my_strncat(char* dest,const char* src,int sz)
+{
+	assert(dest && src);
+	char* ret = dest;
+	int szod = strlen(dest);
+	char* dests = dest + szod;
+	int i = 0;
+	while (sz--)
+	{
+		if (!(*dests++ = *src++))//*dests,*src!=0时继续
+		return ret;
+	}
+	*dests = '\0';//末尾不上\0
+	return ret;
+
+}
+
+int main()
+{
+	char str1[20] = { "abcdef" };
+	char str2[] = { "ggggbb" };
+	my_strncat(str1, str2, 6);
+	printf("%s", str1);
+	return 0;
+}
