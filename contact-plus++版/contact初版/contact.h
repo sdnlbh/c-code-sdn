5ac02deef57fe6cf296a2a3f3251f5bacf727314
unknown <2496366993@qq.com>
2022-11-13 21:43:29 +0800

    plus++

---
+++ contact-plus++版/contact初版/contact.h
@@ -0,0 +1,50 @@
+#pragma once
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+enum option//转换菜单
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	SORT
+};
+
+#define max 1000//定义常量
+#define max_name 20
+#define max_sex 6
+#define max_tele 13
+#define max_addr 50
+#define lim_sz 3;
+
+struct info//信息结构体
+{
+	char name[max_name];
+	int age;
+	char sex[max_sex];
+	char tele[max_tele];
+	char addr[max_addr];
+};
+
+struct contact//contact结构体  动态版改造
+{
+	struct info* data;
+	int capacity;
+	int sz;
+};
+
+
+void menu();
+void initcontact(struct contact* con);
+void conadd(struct contact* con);
+void print(struct contact* con);
+void modify(struct contact* con);
+void search(struct contact* con);
+void delet(struct contact* con);
+int cmp(const void* e1, const void* e2);
+void save(struct contact* con);
+void read(struct contact* con);
---
+++ contact-plus++版/contact初版/contact主体.c
@@ -0,0 +1,67 @@
+#include"contact.h"
+
+
+int main()
+{
+	int cc = 0;
+	struct contact con;//初始化contact
+	initcontact(&con);
+	read(&con);
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf_s("%d", &cc);
+		system("cls");
+		switch (cc)
+		{
+		case EXIT:
+		{
+			save(&con);
+			if (con.data != NULL)//释放空间
+			{
+				free(con.data);
+				con.data = NULL;
+			}
+			printf("已退出");
+			break;
+		}
+		case ADD:
+		{
+			conadd(&con);
+			break;
+		}
+		case DEL:
+		{
+			delet(&con);
+			break;
+		}
+		case SEARCH:
+		{
+			search(&con);
+			break;
+		}
+		case MODIFY:
+		{
+			modify(&con);
+			break;
+		}
+		case SHOW:
+		{
+			print(&con);
+			break;
+		}
+		case SORT:
+		{
+			qsort(con.data, con.sz, sizeof(con.data[0]), cmp);//按年龄排序降序
+			break;
+		}
+		default:
+		{
+			printf("输入错误\n");
+			break;
+		}
+		}
+	} while (cc);
+	return 0;
+}
