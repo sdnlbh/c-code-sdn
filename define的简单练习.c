27bc1c1bde7c3dfad8e12112079f51b9d5a37a0e
sdnlbh <113505317+sdnlbh@users.noreply.github.com>
2022-11-23 23:24:10 +0800

    Update define的简单练习.c

--- define的简单练习.c
+++ define的简单练习.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#define add(x) printf(#x"*"#x" is %d\n",x+x)
-#define add2(name,num) name##num+=1
+#define add(x) printf(#x"*"#x" is %d\n",x+x)//#号的练习代替变量名
+#define add2(name,num) name##num+=1//##的练习拼凑符号
 int main()
 {
 
